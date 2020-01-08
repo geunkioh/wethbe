2bf8b69f11a566f29a4eb054c174e84bf471e8bc
kunki oh <kunkiya@gmail.com>
2020-01-08 16:34:20 +0900

    #2.24 Log In and User Profile Controller

--- README.md
+++ README.md
@@ -1,1 +1,14 @@
 # WeTube
+
+## Pages:
+
+- [ ] Home
+- [ ] Join
+- [ ] Login
+- [x] Search
+- [ ] User Detail
+- [ ] Edit Profile
+- [ ] Change Password
+- [ ] Upload
+- [ ] Video Detail
+- [ ] Edit Video
---
+++ db.js
@@ -0,0 +1,50 @@
+export const videos = [
+    {
+        id: 324393,
+        title: "video awesome",
+        description: "This is someting I love",
+        views: 24,
+        videoFile: "https://archive.org/download/BigBuckBunny_124/Content/big_buck_bunny_720p_surround.mp4",
+        creator: {
+            id: 121212,
+            name: "Nicolas",
+            email: "noco@las.com"
+        }
+    },
+    {
+        id: 121212,
+        title: "video super",
+        description: "This is someting I love",
+        views: 24,
+        videoFile: "https://archive.org/download/BigBuckBunny_124/Content/big_buck_bunny_720p_surround.mp4",
+        creator: {
+            id: 121212,
+            name: "Nicolas",
+            email: "noco@las.com"
+        }
+    },
+    {
+        id: 556655,
+        title: "video nice",
+        description: "This is someting I love",
+        views: 24,
+        videoFile: "https://archive.org/download/BigBuckBunny_124/Content/big_buck_bunny_720p_surround.mp4",
+        creator: {
+            id: 121212,
+            name: "Nicolas",
+            email: "noco@las.com"
+        }
+    },
+    {
+        id: 898997,
+        title: "video perfect",
+        description: "This is someting I love",
+        views: 24,
+        videoFile: "https://archive.org/download/BigBuckBunny_124/Content/big_buck_bunny_720p_surround.mp4",
+        creator: {
+            id: 121212,
+            name: "Nicolas",
+            email: "noco@las.com"
+        }
+    }
+];
